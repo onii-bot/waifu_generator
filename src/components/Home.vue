2348d7cdefc0b1509c7c9684416ca728a4225a5f
onii <fabianmann9@gmail.com>
2023-01-24 09:09:12 +0545

    flex wrap on reset button

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,7 @@
     <input class="w-96 rounded-sm p-1 h-10 mb-4" type="number" id="textare" placeholder="Enter" v-model="max_count">
     <button @click="fetchAPI" type="button" :class="{'disab':isRunning}"
       class="mt-4 mb-2 rounded-lg mx-1 border-2 text-lg py-1 px-2 text-white hover:bg-white hover:border-black hover:text-black">Search</button>
-      <div class="flex">
+      <div class="flex flex-wrap">
         <h1 class="mt-2 text-yellow-600">Note: Due to the limit on API, each image takes 6 seconds to load</h1>
       <button @click="clearLocalStorage" class="rounded-sm ml-2 mt-2 px-1 border-2 text-sm text-white hover:bg-red-600 hover:border-black hover:text-white">Clear</button>  
       </div>
